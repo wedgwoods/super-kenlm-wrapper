333c3b8fc16c76124cd8b801c3fd439b48a2b0fe
wedgwoods <162360302+wedgwoods@users.noreply.github.com>
2023-02-17 14:59:08 +0000

    Remove console statement.

--- src/main/java/kenlm/KenlmJNI.java
+++ src/main/java/kenlm/KenlmJNI.java
@@ -8,3 +8,4 @@ class KenlmJNI {
         try {
             System.load(NativeLibLoader.createTempFileFromResource("/" + System.mapLibraryName("kenlm_jni")));
         } catch (IOException e) {
+            throw new UnsatisfiedLinkError(e.getMessage())
