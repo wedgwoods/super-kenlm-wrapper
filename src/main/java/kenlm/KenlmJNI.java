b7241d5d555d1a0381643f3511fc31c5a1c2b124
wedgwoods <162360302+wedgwoods@users.noreply.github.com>
2023-02-25 11:44:54 +0000

    clean up code.

--- src/main/java/kenlm/KenlmJNI.java
+++ src/main/java/kenlm/KenlmJNI.java
@@ -8,4 +8,9 @@ class KenlmJNI {
         try {
             System.load(NativeLibLoader.createTempFileFromResource("/" + System.mapLibraryName("kenlm_jni")));
         } catch (IOException e) {
-            throw new UnsatisfiedLinkError(e.getMessage())
+            throw new UnsatisfiedLinkError(e.getMessage());
+        }
+    }
+
+    static native long ConfigNew();
+    static native vo
