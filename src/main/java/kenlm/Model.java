d7591b8da7ba0ac47986d6f5179ac43b70a500f7
wedgwoods <162360302+wedgwoods@users.noreply.github.com>
2023-04-06 12:35:29 +0000

    don't crash

--- src/main/java/kenlm/Model.java
+++ src/main/java/kenlm/Model.java
@@ -50,3 +50,83 @@ public class Model implements AutoCloseable {
      * #Bad: never include <s>, even if bos = False.
      * model.score("<s> this is a sentence", bos = False)
      *
+     * #Good: returns log10 p(a fragment)
+     * model.score("a fragment", bos = False, eos = False)
+     *
+     * #Good: returns log10 p(a fragment </s>)
+     * model.score("a fragment", bos = False, eos = True)
+     *
+     * #Ok, but bad practice: returns log10 p(a fragment </s>)
+     * #Unlike <s>, the end of sentence token </s> can appear explicitly.
+     * model.score("a fragment </s>", bos = False, eos = False)
+     */
+    public float score(String sentence, boolean bos, boolean eos) {
+        return ModelScore(model, sentence, bos, eos);
+    }
+
+    /**
+     * Compute perplexity of a sentence.
+     * @param sentence One full sentence to score.  Do not include <s> or </s>.
+     */
+    public double perplexity(String sentence) {
+        return ModelPerplexity(model, sentence);
+    }
+
+    /**
+     * full_scores(sentence, bos = True, eos = Ture) -> generate full scores (prob, ngram length, oov)
+     * @param sentence is a string (do not use boundary symbols)
+     * @param bos should kenlm add a bos state
+     * @param eos should kenlm add an eos state
+     */
+    public List<FullScoreReturn> fullScores(String sentence, boolean bos, boolean eos) {
+        byte[] ptr = ModelFullScores(model, sentence, bos, eos);
+
+        List<FullScoreReturn> list = new ArrayList<>(ptr.length / FullScoreReturn.SIZE);
+        for (int offset = 0; offset < ptr.length; offset += FullScoreReturn.SIZE) {
+            list.add(new FullScoreReturn(ptr, offset));
+        }
+        return list;
+    }
+
+    /**
+     * Change the given state to a BOS state.
+     */
+    public void beginSentenceWrite(State state) {
+        ModelBeginSentenceWrite(model, state.getCState());
+    }
+
+    /**
+     * Change the given state to a NULL state.
+     */
+    public void nullContextWrite(State state) {
+        ModelNullContextWrite(model, state.getCState());
+    }
+
+    /**
+     * Return p(word|in_state) and update the output state.
+     * Wrapper around model.BaseScore(in_state, Index(word), out_state)
+     * @param in_state the context (defaults to NullContext)
+     * @param word the suffix
+     * @param out_state
+     * @return p(word|state)
+     */
+    public float baseScore(State in_state, String word, State out_state) {
+        return ModelBaseScore(model, in_state.getCState(), word, out_state.getCState());
+    }
+
+    /**
+     * Wrapper around model.BaseScore(in_state, Index(word), out_state)
+     * @param in_state the context (defaults to NullContext)
+     * @param word the suffix
+     * @param out_state
+     * @return FullScoreReturn(word|state)
+     */
+    public FullScoreReturn baseFullScore(State in_state, String word, State out_state) {
+        return new FullScoreReturn(ModelBaseFullScore(model, in_state.getCState(), word, out_state.getCState()), 0);
+    }
+
+    @Override
+    public void close() {
+        ModelDelete(model);
+    }
+}
