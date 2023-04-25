a15c12aaa883633deff7063b6744f16dad2f488e
wedgwoods <162360302+wedgwoods@users.noreply.github.com>
2023-04-25 20:07:49 +0000

    doctest fix

--- src/main/java/kenlm/NativeLibLoader.java
+++ src/main/java/kenlm/NativeLibLoader.java
@@ -7,4 +7,6 @@ class NativeLibLoader {
     /**
      * Create a temp file that copies the resource from current JAR archive
      * <p/>
-     * The file from JAR is copied into syste
+     * The file from JAR is copied into system temp file.
+     * The temporary file is deleted after exiting.
+     * Me
