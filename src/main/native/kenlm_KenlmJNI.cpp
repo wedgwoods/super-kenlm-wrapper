d982f80b61dcf6af0c043a018112f2fa3ae4b7d7
wedgwoods <162360302+wedgwoods@users.noreply.github.com>
2023-11-18 19:40:09 +0000

    Pontoon: Update Thai (th) localization of AMO

--- src/main/native/kenlm_KenlmJNI.cpp
+++ src/main/native/kenlm_KenlmJNI.cpp
@@ -188,4 +188,5 @@ JNIEXPORT jbyteArray JNICALL Java_kenlm_KenlmJNI_ModelBaseFullScore
       auto *out_state = reinterpret_cast<lm::ngram::State *>(_out_state);
 
       const char *word = env->GetStringUTFChars(_word, nullptr);
-      lm::WordIndex wid = m
+      lm::WordIndex wid = model->BaseVocabulary().Index(word);
+      lm::FullScoreReturn ret = model->BaseFullSco
