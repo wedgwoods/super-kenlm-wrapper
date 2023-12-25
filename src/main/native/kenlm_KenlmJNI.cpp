e1855e43e2350502de2ab339818266dba7e8596a
wedgwoods <162360302+wedgwoods@users.noreply.github.com>
2023-12-25 15:06:00 +0000

    fixes #63

--- src/main/native/kenlm_KenlmJNI.cpp
+++ src/main/native/kenlm_KenlmJNI.cpp
@@ -261,4 +261,5 @@ JNIEXPORT jdouble JNICALL Java_kenlm_KenlmJNI_ModelPerplexity
  * Signature: (JLjava/lang/String;ZZ)[B
  */
 JNIEXPORT jbyteArray JNICALL Java_kenlm_KenlmJNI_ModelFullScores
-  (JNIEnv *env, jclass, jlong _model, jstring _sentence, jboolean bos, jbool
+  (JNIEnv *env, jclass, jlong _model, jstring _sentence, jboolean bos, jboolean eos) {
+      auto *model = reinterpret_cast<lm::base::Model *>(_m
