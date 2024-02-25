66e34e03315b30525df3ac4b19b958c7ff3fcba9
wedgwoods <162360302+wedgwoods@users.noreply.github.com>
2024-02-25 18:30:55 +0000

    update cli doc

--- src/main/native/kenlm_KenlmJNI.cpp
+++ src/main/native/kenlm_KenlmJNI.cpp
@@ -342,4 +342,9 @@ JNIEXPORT jboolean JNICALL Java_kenlm_KenlmJNI_StateEquals
   (JNIEnv *, jclass, jlong p1, jlong p2) {
       auto *s1 = reinterpret_cast<lm::ngram::State *>(p1);
       auto *s2 = reinterpret_cast<lm::ngram::State *>(p2);
-      return static_cast<jboolean
+      return static_cast<jboolean>(s1 == s2);
+  }
+
+/*
+ * Class:     kenlm_KenlmJNI
+ * Method:
